d4c2de0fec0bb1ffe36f6910f8966798b4fdb306
Kelly Pointer <kellyjoanpointer@gmail.com>
2025-04-10 07:24:07 -0500

    first commit

--- index.js
+++ index.js
@@ -43,5 +43,26 @@ const bookStore = {
     ]
 }
 
-// Write your code here!
+const bookStoreTitle = document.querySelector("#header")
+bookStoreTitle.textContent = bookStore.name
+
+bookStore.books.forEach((book) => {
+
+    const bookContainer = document.createElement("li")
+    const bookTitle = document.createElement("h3")
+    const bookAuthor = document.createElement("p")
+    const bookImage = document.createElement("img")
+    
+    bookTitle.textContent = book.title
+    bookAuthor.textContent = book.author
+    bookImage.src = book.imageUrl
+
+    bookContainer.append(bookTitle)
+    bookContainer.append(bookAuthor)
+    bookContainer.append(bookImage)
+    
+    const bookList = document.querySelector("#book-list")
+    bookList.append(bookContainer)
+
+});
 
